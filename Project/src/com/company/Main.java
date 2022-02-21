dd6e388df67e47843a829fc9ed7559353efadf56
Kassen134340 <99317091+Kassen134340@users.noreply.github.com>
2022-02-22 01:24:30 +0600

    Add files via upload

---
+++ Project/src/com/company/Main.java
@@ -0,0 +1,63 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+    public void main(String[] args) {
+        ArrayList<english> englishQuestions= new ArrayList<>();
+        englishQuestions.add(new english("Please don't forget to ring me when you _______ home",
+                "1)get",
+               "2)are going to get",
+                "3)will get",
+                3));
+        englishQuestions.add(new english("The police are ________ an investigation into the robbery.",
+                "1) working out",
+                "2)carrying out",
+                "3) searching out",
+                3));
+        englishQuestions.add(new english("It's a great place to live apart from the increasing volume of ________ that passes under my window every day.",
+                "1) traffic",
+                "2)transport",
+                "3)vehicles",
+                2));
+        englishQuestions.add(new english("When Sam was a small child, he ________ spend hours every day playing with stones in the garden.",
+                "1)would",
+                "2)used",
+                "3)  was",
+                2));
+        englishQuestions.add(new english("We may have a picnic tomorrow but it will depend ________ the weather.",
+                "1)on",
+                "2)in",
+                "3)0ff",
+                1));
+        englishQuestions.add(new english("I've only got a ________ money in the bank.",
+                "1) less",
+                "2) few",
+                "3)bit",
+                3));
+        englishQuestions.add(new english("You must come quickly now or we won't ________ the bus.",
+                "1) meet",
+                "2)catch",
+                "3) take",
+                2));
+        englishQuestions.add(new english("Have you been back to the house ________ you grew up?",
+                "1) who",
+                "2)where",
+                "3)which",
+                2));
+        englishQuestions.add(new english("What _____are you listening this days?",
+                "1)   because",
+                "2)do",
+                "3) kind",
+                2));
+        englishQuestions.add(new english("Carlos usually gets good marks at school ________ he doesn't seem to spend much time studying.",
+                "1) because",
+                "2)unless",
+                "3)even",
+                2));
+
+    }
+
+
+    }
+
