83535461d61756de6009d74954ae361ca422c629
Asus <213006@astanait.edu.kz>
2022-02-22 17:44:09 +0600

    .

---
+++ src/com/company/Driver.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Driver {
+
+    public static void main(String[] args) {
+//        Game g = new Game();
+//        g.startquiz();
+        StartGame app = new StartGame();
+        app.start();
+    }
+}
