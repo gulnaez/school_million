aa4c06582def71fe4bd2aaa5bc07aeae2d2679bf
gulnaez <213023@astanait.edu.kz>
2022-02-24 17:41:39 +0600

    .

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+//        Game g = new Game();
+//        g.startquiz();
+        StartGame app = new StartGame();
+        app.start();
+    }
+}
